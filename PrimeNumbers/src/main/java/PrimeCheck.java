9a6486a63397b5e40da2d4f2405f43ff1c90fa12
spooky-soup <wtfdashaaa@gmail.com>
2022-03-02 06:14:22 +0700

    PrimeNumbers w/o tests and graphs; .gitignore added

---
+++ PrimeNumbers/src/main/java/PrimeCheck.java
@@ -0,0 +1,14 @@
+public class PrimeCheck {
+    public static boolean isNotPrime(Integer n) {
+        if (n == 1 || n == 2) {
+            return false;
+        }
+        if ((n % 2) == 0) {
+            return true;
+        }
+        for (int i = 3; ((long) i * i) <= n; i += 2) {
+            if (n % i == 0) return true;
+        }
+        return false;
+    }
+}
